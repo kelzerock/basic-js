15a4d43760bfd205af6570a51e8f8fa510e4031a
kelzerock <kelzzzer@gmail.com>
2021-03-06 16:21:34 +0300

    feat: add function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(arr) {
+  if(Array.isArray(arr)){
+  let out = arr.some(item => {  
+    if(typeof(item)==='string') {
+        return true
+      } else {return false}})
+      // console.log(out)
+    } else {return false}
+    // if(out === false){return false} else {}
+
+    let newArr = arr.filter(item => {
+      return typeof (item) === 'string'
+    })
+    // console.log(newArr)
+    let newArr1 = newArr.map(item =>{
+      return item.toUpperCase().trim()
+    })
+    // console.log(newArr1)
+    newArr1.sort()
+    // console.log(newArr1)
+    let arrLit= newArr1.map(item =>{
+      return item[0]
+    })
+  
+    return arrLit.join('')
+  };
