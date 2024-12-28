590c4e9cd0951132ab4594b4a09f5a1ef358ef23
Aleksei Zhuchkov <kelzzzer@gmail.com>
2024-12-29 01:53:48 +0300

    feat: complete task vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,88 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction = true) {
+    this.directionMachine = direction;
+    this.firstCharCodeUK = 65;
+    this.lastCharCodeUK = 90;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    let result = "";
+    let textUpperCase = text.toUpperCase();
+    let keyUpperCase = key.toUpperCase();
+
+    let count = 0;
+    for (let i = 0; i < text.length; i += 1) {
+      if (
+        textUpperCase[i].charCodeAt() > this.lastCharCodeUK ||
+        textUpperCase[i].charCodeAt() < this.firstCharCodeUK
+      ) {
+        result += textUpperCase[i];
+      } else {
+        let codeText = textUpperCase[i].charCodeAt() - this.firstCharCodeUK;
+        let codeKey = keyUpperCase[count].charCodeAt() - this.firstCharCodeUK;
+        let codeMachine = 0;
+        if (codeText + codeKey > this.lastCharCodeUK - this.firstCharCodeUK) {
+          codeMachine =
+            this.firstCharCodeUK +
+            codeText +
+            codeKey -
+            (this.lastCharCodeUK - this.firstCharCodeUK) -
+            1;
+        } else {
+          codeMachine = this.firstCharCodeUK + codeText + codeKey;
+        }
+        result += String.fromCharCode(codeMachine);
+        count += 1;
+        if (count >= keyUpperCase.length) count = 0;
+      }
+    }
+    if (this.directionMachine) {
+      return result;
+    } else {
+      return result.split("").reverse().join("");
+    }
   }
+
+  decrypt(text, key) {
+    if (!text || !key) throw new Error('Incorrect arguments!');
+    let result = "";
+    let textUpperCase = text.toUpperCase();
+    let keyUpperCase = key.toUpperCase();
+
+    let count = 0;
+    for (let i = 0; i < text.length; i += 1) {
+      if (
+        textUpperCase[i].charCodeAt() > this.lastCharCodeUK ||
+        textUpperCase[i].charCodeAt() < this.firstCharCodeUK
+      ) {
+        result += textUpperCase[i];
+      } else {
+        let codeText = textUpperCase[i].charCodeAt() - this.firstCharCodeUK;
+        let codeKey = keyUpperCase[count].charCodeAt() - this.firstCharCodeUK;
+        let codeMachine = 0;
+        if (codeText - codeKey < 0) {
+          codeMachine =
+            this.lastCharCodeUK - Math.abs(codeText - codeKey) + 1;
+        } else {
+          console.log("xx")
+          codeMachine = this.firstCharCodeUK + codeText - codeKey;
+        }
+        console.log({codeMachine, codeText, codeKey})
+        result += String.fromCharCode(codeMachine);
+        count += 1;
+        if (count >= keyUpperCase.length) count = 0;
+      }
+    }
+    if (this.directionMachine) {
+      return result;
+    } else {
+      return result.split("").reverse().join("");
+    }
+  }
+
 }
 
 module.exports = {
